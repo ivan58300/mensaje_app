590db29ffba441f3ff169d4a85d7bfd09bd6b83e
ivan58300 <59028802+ivan58300@users.noreply.github.com>
2021-11-10 18:07:31 -0600

    agregamos la funcion enviar  mensaje

--- src/main/java/com/platzi/mensaje_app/OADMensaje.java
+++ src/main/java/com/platzi/mensaje_app/OADMensaje.java
@@ -3,6 +3,7 @@ package com.platzi.mensaje_app;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 
 //objeto de acceso a datos de mensaje
 public class OADMensaje {
@@ -31,14 +32,79 @@ public class OADMensaje {
     }
 
     public static void leerMensajeBD(){
+        Conexion db_connect = new Conexion();
+
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try(Connection conexion = db_connect.getConexion())  {
+            String query="SELECT * FROM mensajes";
+            ps = conexion.prepareStatement(query);
+            rs = ps.executeQuery();
+
+            //mostrar los datos del mensaje
+            while(rs.next()){
+                System.out.println("ID: "+rs.getInt("id_mensaje"));
+                System.out.println("Mensaje: "+rs.getString("mensaje"));
+                System.out.println("Autor: "+rs.getString("autor_mensaje"));
+                System.out.println("Fecha: "+rs.getString("fecha_mensaje"));
+                System.out.println("");
+            }
+        }catch(SQLException e){
+            System.out.println("no se pudieron recuperar los mensajes");
+            System.out.println(e);
+        }
 
     }
 
     public static void borrarMensajeBD(int id_mensaje){
+        Conexion db_connect = new Conexion();
+        //El try catch es para validar si tenemos una conexión exitosa
+        try(Connection conexion = db_connect.getConexion())  {
+            //preparar la sentencia
+            PreparedStatement ps =null;
+            //consultar base de datos
+            try {
+                String query="DELETE FROM mensajes WHERE id_mensaje = ?";
+                ps=conexion.prepareStatement(query);
+                ps.setInt(1, id_mensaje);
+                //operacion de transacción que borra un dato de la base de datos
+                ps.executeUpdate();
+                System.out.println("el mensaje ha sido borrado");
+            }catch(SQLException e) {
+                //mostrar la exepcion
+                System.out.println(e);
+                System.out.println("no se pudo borrar el mensaje");
+            }
+
+
+        }catch(SQLException e){
+            System.out.println(e);
+        }
 
     }
 
     public static void actualizarMensajeBD(Mensaje mensaje){
+        Conexion db_connect = new Conexion();
 
+        try(Connection conexion = db_connect.getConexion())  {
+            PreparedStatement ps = null;
+
+            try{
+                //actualizar base de datos
+                String query="UPDATE mensajes SET mensaje = ? WHERE id_mensaje = ?";
+                ps=conexion.prepareStatement(query);
+                ps.setString(1, mensaje.getMensaje());
+                ps.setInt(2, mensaje.getId_mensaje());
+                //se ejecuta el comando en el base de datos
+                ps.executeUpdate();
+                System.out.println("mensaje se actualizó correctamente");
+            }catch(SQLException ex){
+                System.out.println(ex);
+                System.out.println("no se pudo actualizar el mensaje");
+            }
+        }catch(SQLException e){
+            System.out.println(e);
+        }
     }
 }
--- src/main/java/com/platzi/mensaje_app/ServicioMensaje.java
+++ src/main/java/com/platzi/mensaje_app/ServicioMensaje.java
@@ -19,14 +19,26 @@ public class ServicioMensaje {
     }
 
     public static void listaMensaje(){
-
+        OADMensaje.leerMensajeBD();
     }
 
     public static void borrarMensaje(){
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("indica el ID del mensaje a borrar");
+        int id_mensaje= sc.nextInt();
+        OADMensaje.borrarMensajeBD(id_mensaje);
     }
 
     public static void editarMensaje(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("escribe tu nuevo mensaje");
+        String mensaje = sc.nextLine();
 
+        System.out.println("indica el ID del mensaje a editar");
+        int id_mensaje= sc.nextInt();
+        Mensaje actualizacion = new Mensaje();
+        actualizacion.setId_mensaje(id_mensaje);
+        actualizacion.setMensaje(mensaje);
+        OADMensaje.actualizarMensajeBD(actualizacion);
     }
 }
